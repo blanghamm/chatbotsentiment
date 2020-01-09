19729033edd6601ed7cd1541c15ece51c3392148
Ben Langham <langham1291@outlook.com>
2020-01-09 12:13:40 +0000

    updated

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import './plugins/bootstrap-vue'
+import './plugins/axios'
 import App from './App.vue'
 
 Vue.config.productionTip = false
